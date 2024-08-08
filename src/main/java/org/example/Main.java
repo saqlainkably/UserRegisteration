50e77eb1b9d85b15a94c1f32f01a3bbc78e32819
Your Name <saqlainzakaria05@gmail.com>
2024-08-08 13:54:52 +0530

    Added: User's First Name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to User Registration");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first name");
+        String fName=sc.next();
+        String pattern = "^[A-Z][a-zA-Z]{2,}$";
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(fName);
+        System.out.println(matcher.matches());
     }
 }
