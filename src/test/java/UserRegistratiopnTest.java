d0bea0c24992a9d8854d1369862cd68d1f96f88f
Your Name <saqlainzakaria05@gmail.com>
2024-08-08 18:29:24 +0530

    Added: Parameterised Test Updated

--- src/test/java/UserRegistratiopnTest.java
+++ src/test/java/UserRegistratiopnTest.java
@@ -1,6 +1,9 @@
 import org.example.Main;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserRegistratiopnTest {
 
@@ -34,5 +37,20 @@ public class UserRegistratiopnTest {
         String password = Main.fNameValidate("Saqlain");
         Assert.assertEquals("HAPPY", password);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "test.sds@example.co.ij",
+            "user.name+tag+sorting@example.com",
+            "user.name@example.co.uk",
+//            "invalid-email@",
+//            "@example.com",
+//            "user@.com.my"
+    })
+    void validEmail (String mail){
+        String result= Main.emailValidate(mail);
+        Assert.assertEquals("HAPPY",result);
+    }
+
 
 }
