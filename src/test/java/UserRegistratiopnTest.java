84966407089dd8983eed2e4272ec8eba391e71cc
Your Name <saqlainzakaria05@gmail.com>
2024-08-09 10:35:58 +0530

    Added: Testing for User Registration

--- src/test/java/UserRegistratiopnTest.java
+++ src/test/java/UserRegistratiopnTest.java
@@ -1,9 +1,6 @@
 import org.example.Main;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserRegistratiopnTest {
 
@@ -16,41 +13,25 @@ public class UserRegistratiopnTest {
 
     @Test
     public void lnameTest() {
-        String lname = Main.fNameValidate("Kably");
+        String lname = Main.lNameValidate("Kably");
         Assert.assertEquals("HAPPY", lname);
     }
 
     @Test
     public void emailTest() {
-        String email = Main.fNameValidate("Saqlain");
+        String email = Main.emailValidate("Saqlain.kablyy1@gmail.com");
         Assert.assertEquals("HAPPY", email);
     }
 
     @Test
     public void phoneNumberTest() {
-        String phoneNumber = Main.fNameValidate("Saqlain");
+        String phoneNumber = Main.phoneNumberValidate("91 8555829900");
         Assert.assertEquals("HAPPY", phoneNumber);
     }
 
     @Test
     public void passwordTest() {
-        String password = Main.fNameValidate("Saqlain");
+        String password = Main.passwordValidate("Saqlain@123");
         Assert.assertEquals("HAPPY", password);
     }
-
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "test.sds@example.co.ij",
-            "user.name+tag+sorting@example.com",
-            "user.name@example.co.uk",
-//            "invalid-email@",
-//            "@example.com",
-//            "user@.com.my"
-    })
-    void validEmail (String mail){
-        String result= Main.emailValidate(mail);
-        Assert.assertEquals("HAPPY",result);
-    }
-
-
 }
