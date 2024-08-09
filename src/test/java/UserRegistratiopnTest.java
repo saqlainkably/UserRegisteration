238a731cca9a7ba9323c1f864e23b321e2e05851
Your Name <saqlainzakaria05@gmail.com>
2024-08-09 10:42:37 +0530

    Added: Parameterized Test Added for User's Email Validation

--- src/test/java/UserRegistratiopnTest.java
+++ src/test/java/UserRegistratiopnTest.java
@@ -1,6 +1,9 @@
 import org.example.Main;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserRegistratiopnTest {
 
@@ -34,4 +37,19 @@ public class UserRegistratiopnTest {
         String password = Main.passwordValidate("Saqlain@123");
         Assert.assertEquals("HAPPY", password);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "test.sds@example.co.ij",
+            "user.name+tag+sorting@example.com",
+            "user.name@example.co.uk",
+//            "invalid-email@",
+//            "@example.com",
+//            "user@.com.my"
+    })
+    void validEmail (String mail){
+        String result= Main.emailValidate(mail);
+        Assert.assertEquals("HAPPY",result);
+    }
+
 }
